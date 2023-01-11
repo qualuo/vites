31addae0d55e509c46a9dee98b46c49f026b6942
Quang Luong <qualuo@gmail.com>
2023-01-11 15:19:06 +0100

    Push pixels

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,52 +1,39 @@
 import React from 'react';
-import HeroHeader from './HeroHeader'
-
-
 
 function HomePage() {
   return (
     
-    <div className="bg-black-200">
+    <div className="container mx-auto font-serif text-center text-gray-900 text-2xl">
 
-      
-    <div className="bg-cover bg-center h-64" style={{backgroundImage: "url(https://via.placeholder.com/2000x600.png?text=Hero+Image)"}}>
-        
-        <div className="container mx-auto h-full flex items-center text-center">
-          <div className="w-full">
-  
-            <h1 className="text-6xl font-medium text-white leading-none mb-4">Welcome to My Website</h1>
-            <h2 className="text-4xl font-medium text-white leading-none mb-4">
-              <span className="text-gradient">Explore</span>
-              <span className="text-gradient">Discover</span>
-              <span className="text-gradient">Learn</span>
-            </h2>
-  
-          </div>
+      <nav>
+        <div className="p-6">
+          <p className="text-xl text-gray-700">Home</p>
         </div>
-      </div>
-        
-      <header className="bg-white p-6">
-        <h1 className="text-3xl font-bold">In need of a world-class software engineer.</h1>
+      </nav>
+
+      <header className="p-28">
+        <h1 className="text-9xl p-12 text-center text-gray-900">KINGSMAN</h1>
+        <h2 className="text-3xl p-2 text-center text-gray-500">World class premium solutions of the highest quality</h2>
       </header>
 
-      <section className="bg-white p-12">
-        <h2 className="text-2xl font-medium mb-4">About Us</h2>
-        <p className="text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna vel bibendum luctus, velit elit bibendum mi, at maximus mi elit a nunc. Nulla facilisi.
+      <section className="p-12">
+        <h2 className="text-4xl font-medium mb-4">About</h2>
+        <p className="text-gray-700">Indulge in the exclusivity of our bespoke tailoring service and create a one-of-a-kind wardrobe that is tailored to your unique style.
         </p>
       </section>
       
-      <section className="bg-white p-6">
-        <h2 className="text-2xl font-medium mb-4">Our Products</h2>
-        <ul>
-          <li className="text-gray-700 mb-2">Product 1</li>
-          <li className="text-gray-700 mb-2">Product 2</li>
-          <li className="text-gray-700 mb-2">Product 3</li>
+      <section className="p-6">
+        <h2 className="text-3xl font-medium mb-4">Our Products</h2>
+        <ul className="text-gray-500">
+          <li className="p-12">Step into the world of refinement and elegance with our <br/>curated selection of diamond jewelry, made with only the finest quality gems.</li>
+          <li className="p-12">Join the ranks of the world's elite with our collection of luxury watches, featuring exclusive designs and state-of-the-art technology</li>
+          <li className="p-12">Elevate your home with our collection of designer furniture, featuring timeless pieces from the most iconic names in the industry</li>
+          <li className="p-12">Travel in style with our collection of luxury luggage, made from the finest quality materials and designed to withstand the rigors of the road.</li>
         </ul>
       </section>
 
-      <footer className="bg-white p-6">
-        <p className="text-gray-700">Copyright © 2021 My Landing Page</p>
+      <footer className="bg-white p-6 bg-gradient-to-r from-amber-900 to-orange-100">
+        <p className="text-1xl text-gray-700">Amber</p>
       </footer>
 
     </div>
